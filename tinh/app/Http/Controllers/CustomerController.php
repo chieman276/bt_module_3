83f2d79044a3665da27d92061d0002bb3f42a24d
chieman276 <chieman2k3@gmail.com>
2022-02-10 11:45:27 +0700

    tỉnh thành phố

---
+++ tinh/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $cities = City::all();
+        $customers = Customer::all();
+        return view('customers.list', compact('customers','cities'));
+        
+    }
+    public function create()
+    {
+        $cities = City::all();
+        return view('customers.create', compact('cities'));
+    }
+
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->dob = $request->input('dob');
+        $customer->city_id = $request->input('city_id');
+        $customer->save();
+        Session::flash('success', 'Tạo mới khách hàng thành công');
+        return redirect()->route('customers.index');
+    }
+    public function edit($id)
+    {
+        //tìm id nếu id ko có thì báo lỗi 400
+        $customer = Customer::findOrFail($id);
+        //lấy tất cả thông tin của bảng cities
+        $cities = City::all();
+
+        return view('customers.edit', compact('customer', 'cities'));
+    }
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->dob = $request->input('dob');
+        $customer->city_id = $request->input('city_id');
+        $customer->save();
+        //dùng để thông báo ở view
+        Session::flash('success', 'Cập nhật khách hàng thành công');
+
+        return redirect()->route('customers.index');
+    }
+    //tiêm vào dependency injection để không cần khởi tạo lại lớp đó nữa
+    public function filterByCity(Request $request)
+    {
+        
+        $cities = City::all();
+        $customers = Customer::where('city_id', '=', $request->city_id)->get();
+        return view('customers.list', compact('customers','cities'));
+
+    }
+}
